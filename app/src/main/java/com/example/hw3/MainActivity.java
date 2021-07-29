fddd39d1593e3ba9a9a913733ccd5e341a52ecda
shalva <smkhetsadze@gmail.com>
2021-07-29 22:36:21 +0400

    Initial commit

---
+++ app/src/main/java/com/example/hw3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hw3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.sip.SipSession;
+import android.os.Bundle;
+
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
